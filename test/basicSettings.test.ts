ee9a1c9fb9f59f88bbb7936e3ffc9b73a8b5da84
penielny <penielnyinaku@gmail.com>
2025-05-15 15:40:35 +0000

    update: created a test file for the advanceSettings file

--- test/basicSettings.test.ts
+++ test/basicSettings.test.ts
@@ -23,28 +23,6 @@ describe('Light class', () => {
     });
 
     it('should get the selected component name', () => {
-
-        let htmlString = `
-            <div class="rooms bedroom">
-                <img src="./assets/imgs/bedroom.jpg" alt="image of a bedroom">
-                <p>bedroom</p>
-                <div class="basic_settings">
-                    <section class="slider">
-                        <input type="range" name="light intensity" id="light_intensity" value="0" min="0" max="10">
-                    </section>
-                    <section class="basic_settings_buttons">
-                        <button class="light-switch">
-                            <img src="./assets/svgs/light_bulb_off.svg" data-lightOn="./assets/svgs/light_bulb.svg" alt="fullscreen button svg icon">
-                        </button>
-
-                        <button class="advance-settings_modal">
-                            <img src="./assets/svgs/enlarge.svg" alt="fullscreen button svg icon">
-                        </button>
-
-                    </section>
-                </div>
-            </div>
-            `;
         const mockParentElement = document.createElement('div');
         const mocChildElement = document.createElement('p');
         mocChildElement.textContent = 'guest room';
